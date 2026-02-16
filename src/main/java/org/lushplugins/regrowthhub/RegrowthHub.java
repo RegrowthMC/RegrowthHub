7661dcffbf8087003b4c10fa92fb0b22146462b0
OakLoaf <oak@beaconstudios.org>
2026-02-16 20:59:48 +0000

    Moved command to Lamp

--- src/main/java/org/lushplugins/regrowthhub/RegrowthHub.java
+++ src/main/java/org/lushplugins/regrowthhub/RegrowthHub.java
@@ -1,20 +1,14 @@
 package org.lushplugins.regrowthhub;
 
 import org.bukkit.Bukkit;
-import org.lushplugins.lushlib.LushLib;
 import org.lushplugins.lushlib.plugin.SpigotPlugin;
 import org.lushplugins.regrowthhub.command.PlayChessCommand;
 import org.lushplugins.regrowthhub.module.staticeffects.DuckyRainbowTask;
 import org.lushplugins.regrowthhub.module.staticeffects.PicnicHeartsTask;
 import org.lushplugins.regrowthhub.module.swaphandfix.SwapHandFixListener;
+import revxrsal.commands.bukkit.BukkitLamp;
 
-// TODO: Consider merging into main Regrowth plugin as modules
 public final class RegrowthHub extends SpigotPlugin {
-
-    @Override
-    public void onLoad() {
-        LushLib.getInstance().enable(this);
-    }
 
     @Override
     public void onEnable() {
@@ -22,14 +16,11 @@ public final class RegrowthHub extends SpigotPlugin {
             new SwapHandFixListener()
         );
 
-        registerCommand(new PlayChessCommand());
-
         Bukkit.getScheduler().runTaskTimer(this, new DuckyRainbowTask(), 20, 100);
         Bukkit.getScheduler().runTaskTimer(this, new PicnicHeartsTask(), 20, 1200);
-    }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        BukkitLamp.builder(this)
+            .build()
+            .register(new PlayChessCommand());
     }
 }
--- src/main/java/org/lushplugins/regrowthhub/command/PlayChessCommand.java
+++ src/main/java/org/lushplugins/regrowthhub/command/PlayChessCommand.java
@@ -1,39 +1,16 @@
 package org.lushplugins.regrowthhub.command;
 
-import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.lushplugins.lushlib.command.Command;
-import org.lushplugins.lushlib.libraries.chatcolor.ChatColorHandler;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.bukkit.actor.BukkitCommandActor;
 
-import java.util.List;
+@Command("playchess")
+@SuppressWarnings("unused")
+public class PlayChessCommand {
 
-public class PlayChessCommand extends Command {
-
-    public PlayChessCommand() {
-        super("playchess");
-    }
-
-    @Override
-    public boolean execute(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String label, @NotNull String[] args, @NotNull String[] fullArgs) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("Command must be ran by a player");
-            return true;
-        }
-
-        if (args.length == 0) {
-            ChatColorHandler.sendMessage(sender, "&#ff6969Incorrect usage, try &#d13636/playchess <player>");
-            return true;
-        }
-
-        player.performCommand(String.format("chess challenge player LobbyBoard %s white", args[0]));
-        return true;
-    }
-
-    @Override
-    public @Nullable List<String> tabComplete(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String label, @NotNull String[] args, @NotNull String[] fullArgs) {
-        return args.length == 1 ? Bukkit.getOnlinePlayers().stream().map(Player::getName).toList() : null;
+    @Command("playchess")
+    public void playChess(BukkitCommandActor actor, Player target) {
+        Player player = actor.requirePlayer();
+        player.performCommand(String.format("chess challenge player LobbyBoard %s white", target.getName()));
     }
 }
